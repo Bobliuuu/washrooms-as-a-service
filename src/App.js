b536bdcb5c31f546e8d8c81bb86a53194dddefe9
bennywu2019 <benny.wu.new@gmail.com>
2022-12-04 00:40:21 -0500

    what is react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Map, MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useRef, useState, useEffect } from "react";
+import { Map, MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import { GenerateForm } from "./components/form/GenerateForm";
 import { CreateNavBar } from "./components/nav-bar/CreateNavBar";
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 import About from "./pages/About";
+import axios from "axios";
 
 let DefaultIcon = L.icon({
   iconUrl: icon,
-  shadowUrl: iconShadow
+  shadowUrl: iconShadow,
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
@@ -20,33 +21,47 @@ L.Marker.prototype.options.icon = DefaultIcon;
 const defaultCenter = [38.9072, -77.0369];
 const defaultZoom = 8;
 const disneyWorldLatLng = [28.3852, -81.5639];
-const disneyLandLatLng = [33.8121, -117.9190];
+const disneyLandLatLng = [33.8121, -117.919];
 
 function App() {
-  
-  {/*
+  const [markers, setMarkers] = useState([
+    [355, 234],
+    [13, 34],
+  ]);
+
+  const [count, setCount] = useState(0);
   useEffect(() => {
     alert("yes");
-    
-    fetch("https://api-adresse.data.gouv.fr/search/?q=paris&type=street")
-      .then((response) => response.json())
-      .then((response) => {
-        alert(response.features[0].geometry.coordinates[0]);
-        this.setMarkers(response.features);
-      });
- 
-  }, []); */}
+
+    const fetchData = async () => {
+      const result = await axios(
+        "https://api-adresse.data.gouv.fr/search/?q=paris&type=street"
+      );
+
+      const marker = [
+        ...markers,
+        [
+          result.data.features[0].geometry.coordinates[0],
+          result.data.features[0].geometry.coordinates[1],
+        ],
+      ];
+      setMarkers(marker);
+      handleOnSetView();
+    };
+    fetchData();
+  }, []);
 
   const mapRef = useRef();
 
   /**
- * handleOnSetView
- */
+   * handleOnSetView
+   */
 
   function handleOnSetView() {
     const { current = {} } = mapRef;
     const { leafletElement: map } = current;
-    map.setView(disneyWorldLatLng, 14);
+
+    map.setView(markers[1], 14);
   }
 
   /**
@@ -57,67 +72,65 @@ function App() {
     const { current = {} } = mapRef;
     const { leafletElement: map } = current;
 
-    map.flyTo(disneyLandLatLng, 14, {
-      duration: 2
+    map.flyTo(markers[1], 14, {
+      duration: 2,
     });
   }
 
-  const [markers, setMarkers] = useState([[355, 234], [13, 34]]);
-
   return (
     <div className="App">
-        <CreateNavBar />
-        <Router>
+      <CreateNavBar />
+      <Router>
         <Routes>
-        {/* <Route exact path="/" element={<Homepage />}/>  */}
-        <Route path="/form" element={<GenerateForm />} />
+          {/* <Route exact path="/" element={<Homepage />}/>  */}
+          <Route path="/form" element={<GenerateForm />} />
         </Routes>
-        </Router>
-        <Map ref={mapRef} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          
-          {[[51.505, -0.09], [24.42, -32.24], [-34.45, -3.45]].map((x) => (
-            <Marker
-              position={[x[1], x[0]]}>
-              <Popup>
+      </Router>
+      <Map
+        ref={mapRef}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {[
+          [51.505, -0.09],
+          [24.42, -32.24],
+          [-34.45, -3.45],
+        ].map((x) => (
+          <Marker position={[x[1], x[0]]}>
+            <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
-            </Marker>
-          ))} 
-        </Map>
-        <div className="sidebar">
-          <h2>Disney World</h2>
-          <p>
-            Bay Lake, FL
-          </p>
-          <ul>
-            <li>Lat: 28.3852</li>
-            <li>Long: -81.5639</li>
-          </ul>
-          <p>
-            <button onClick={handleOnSetView}>
-              Set View to Disney World
-            </button>
-          </p>
-          <h2>Disneyland</h2>
-          <p>
-            Anaheim, CA
-          </p>
-          <ul>
-            <li>Lat: 33.8121</li>
-            <li>Long: -117.9190</li>
-          </ul>
-          <p>
-            <button onClick={handleOnFlyTo}>
-              Fly to Disneyland
-            </button>
-          </p>
-        </div>
+          </Marker>
+        ))}
+      </Map>
+      <div className="sidebar">
+        <h2>Disney World</h2>
+        <p>Bay Lake, FL</p>
+        <ul>
+          <li>Lat: 28.3852</li>
+          <li>Long: -81.5639</li>
+        </ul>
+        <p>
+          <button onClick={handleOnSetView}>Set View to Disney World</button>
+        </p>
+        <h2>Disneyland</h2>
+        <p>Anaheim, CA</p>
+        <ul>
+          <li>Lat: 33.8121</li>
+          <li>Long: -117.9190</li>
+        </ul>
+        <p>
+          <button onClick={handleOnFlyTo}>Fly to Disneyland</button>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
