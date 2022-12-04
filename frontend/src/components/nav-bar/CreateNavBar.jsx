f23ead03a113639243ef72de79c0f95caa0ab347
Eugene Zhang <zhang.eug@gmail.com>
2022-12-03 20:52:35 -0500

    Add routes

--- frontend/src/components/nav-bar/CreateNavBar.jsx
+++ frontend/src/components/nav-bar/CreateNavBar.jsx
@@ -2,13 +2,15 @@ import "./create-nav-bar.css";
 import React from "react";
 import logo from "../../assets/washroom-as-service.png";
 
+import { Link } from 'react-router-dom';
+
 export const CreateNavBar = () => {
     return (
         <div>
             <nav className="topnav">
                 <div className="topnav-left">
                     <a href="/" className="active"><img className="logo" src={logo} alt="Logo" width={100}></img></a>
-                    <a href="/">Extra</a>
+                    <Link to="/about">Extra</Link>
                 </div>
                 <div className="topnav-right">
                     <a href="/">Submit a Washroom</a>
---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      {/* style={{
+        display: "flex",
+        justifyContent: "Right",
+        alignItems: "Right",
+        height: "100vh",
+      }}
+    >
+      <h1>Stuff here.</h1> */}
+      hello world
+    </div>
+  );
+};
